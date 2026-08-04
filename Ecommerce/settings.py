fb347cbc83e6d039f6c16165f06fb5b6154613f4
Bibekacharya55 <bibekacharya55@gmail.com>
2026-08-04 15:15:47 +0545

    cors added

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+cth2@l0+4n@^b&n2sl^#zpeassfe2h&v_fh$!$(htz&2xzof4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        "corsheaders",
+
     'rest_framework',
     'products',
 ]
@@ -44,6 +46,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+        "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,3 +124,4 @@ STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
+CORS_ALLOWED_ORIGINS=["http://localhost:5173",]
